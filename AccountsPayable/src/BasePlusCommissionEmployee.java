e0c05da3661b1bd42a72aa0b6189ee502e2fee91
Willits <S02907014@ccc.ccofc.edu>
2023-01-26 10:53:00 -0700

    update

--- AccountsPayable/src/BasePlusCommissionEmployee.java
+++ AccountsPayable/src/BasePlusCommissionEmployee.java
@@ -1,4 +1,30 @@
 
 public class BasePlusCommissionEmployee extends CommissionEmployee {
+	private double basePay;
+	
+	public BasePlusCommissionEmployee(String first, String last, int SSN, double grossSales, double commission, double basePay) {
+		super(first, last, SSN, grossSales, commission);
+		this.setBasePay(basePay);
+	}
 
+	public double getBasePay() {
+		return basePay;
+	}
+
+	public void setBasePay(double basePay) {
+		this.basePay = basePay;
+	}
+
+	@Override
+	public double getPaymentAmount() {
+		// TODO Auto-generated method stub
+		return super.getPaymentAmount() + this.basePay;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	
 }
