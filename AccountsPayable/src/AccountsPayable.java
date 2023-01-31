4cac4b6d953ad5e3a54b5ccff399ba651ba2f001
Willits <S02907014@ccc.ccofc.edu>
2023-01-31 09:55:55 -0700

    Nearly finished accounts payable assignment

--- AccountsPayable/src/AccountsPayable.java
+++ AccountsPayable/src/AccountsPayable.java
@@ -3,7 +3,19 @@ public class AccountsPayable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Employee[] payableEmployees = new Employee[6];
 		
+		for (Employee Employees: payableEmployees) {
+			System.out.println("FIX ME");
+		}
+	}
+	
+	public void print (Employee employee) {
+		System.out.println(employee.toString());
+	}
+	
+	public void otherPrint (Employee employee) {
+		System.out.println("first = " + employee.getFirst()+ "\nlast = " + employee.getLast() + "\nSSN = " + employee.getSSN());
 	}
 
 }
