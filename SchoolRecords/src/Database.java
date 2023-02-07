16e8fd523b9db1ac9d8b35bd9e9b4b32b25b13eb
Willits <S02907014@ccc.ccofc.edu>
2023-02-07 10:52:37 -0700

    worked on file i/o

--- SchoolRecords/src/Database.java
+++ SchoolRecords/src/Database.java
@@ -1,36 +1,45 @@
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Database {
 	
-	private final String FILE_NAME = "PersonDatabase";
+	private final String FILE_NAME = "PersonDatabase.txt";
+	//private FileWriter fWriter = 
 
 	public Database() throws IOException {
 		// TODO Auto-generated constructor stub
-		File myFile = new File("PersonDatabase.txt");
+		File myFile = new File(FILE_NAME);
 		
 		if (!myFile.exists()) {
 			if (myFile.createNewFile()) {
 				System.out.println("Created new file");
 			}
 		}
-		PrintWriter printWriter = new PrintWriter(myFile.getName());
+		
+		PrintWriter writer = new PrintWriter(myFile.getName());
+		writer.close();
 		
 	}
 	
-	public void writePerson (Person person) {
+	public void writePerson (Person person) throws IOException {
 		
+		PrintWriter writer = new PrintWriter(new FileOutputStream(FILE_NAME, true));
+		
+		writer.println(person.getClass());
+		writer.println(person.toString());
+		
+		writer.close();
 	}
 	
-//	File myFile = new File("filewritetest.txt");
-//	if (!myFile.exists()) {
-//		if (myFile.createNewFile()) {
-//			
-//		}
-//	}
-//	
-//	PrintWriter printWriter = new PrintWriter(myFile.getName());
-//	printWriter.close();
+	public ArrayList<Person> readDatabase () {
+		ArrayList<Person> data = new ArrayList<Person>();
+		
+		return data;
 
+	}
+	
 }
--- SchoolRecords/src/TestSchoolRecords.java
+++ SchoolRecords/src/TestSchoolRecords.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TestSchoolRecords {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Database test = new Database();
-
+		Person person = new Person();
+		Person person2 = new Person();
+		
+		test.writePerson(person);
+		test.writePerson(person2);
 	}
 
 }
