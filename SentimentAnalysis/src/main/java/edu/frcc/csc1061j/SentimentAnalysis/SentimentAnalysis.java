c157c30ed4286e87ec69dfd8aa11a9439a6ac412
Gray Willits <jacobpwillits@gmail.com>
2023-03-20 11:25:51 -0600

    finished simple sentiment analysis

--- SentimentAnalysis/src/main/java/edu/frcc/csc1061j/SentimentAnalysis/SentimentAnalysis.java
+++ SentimentAnalysis/src/main/java/edu/frcc/csc1061j/SentimentAnalysis/SentimentAnalysis.java
@@ -9,21 +9,51 @@ public class SentimentAnalysis {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		Scanner scnr = new Scanner(System.in);
 		HashMap<String, Integer> sentiments = new HashMap<String, Integer>();
-		String[] temp;
-		
+		String[] data;
+		String[] words;
+		boolean first = true;
+		int sentiment = 0;
+		double overall = 0;
+		String input = "";
+		Scanner scnr = new Scanner(System.in);
 		File file = new File("sentiments.txt");
 		Scanner reader = new Scanner(file);
 		
 		while (reader.hasNextLine()) { //Apparently "naive" is in there twice
-			temp = reader.nextLine().split(",");
-			sentiments.put(temp[0], Integer.parseInt(temp[1]));
+			data = reader.nextLine().split(",");
+			sentiments.put(data[0], Integer.parseInt(data[1]));
 		}
 		
+		System.out.println("Enter text:");
+		while (true) {
+			String temp = scnr.nextLine();
+			if (temp.equals("END")) {
+				break;
+			}
+			input += ((first) ? "" : " ") + temp;
+			first = false;
+		}
+		words = input.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+		System.out.println("\nWords: " + words.length);
+		for (int i = 0; i < words.length; i++) {
+			if (i < words.length - 1) {
+				if (sentiments.containsKey(words[i] + " " + words[i + 1])) {
+					sentiment += sentiments.get(words[i] + " " + words[i + 1]);
+					continue;
+				}
+			}
+			if (sentiments.containsKey(words[i])) {
+				sentiment += sentiments.get(words[i]);
+				continue;
+			}
+		}
+		System.out.println("Sentiment: " + sentiment);
+		overall = (double)sentiment / words.length;
+		System.out.printf("Overall: %.2f", overall);
 		
 		reader.close();
-		
+		scnr.close();
 	}
 
 }
