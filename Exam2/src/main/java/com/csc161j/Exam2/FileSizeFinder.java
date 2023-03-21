8e8b9dfb470c5e4465c8c8e6fa02f93defab056f
Willits <S02907014@ccc.ccofc.edu>
2023-03-21 10:55:23 -0600

    worked on automaticly getting input file

--- Exam2/src/main/java/com/csc161j/Exam2/FileSizeFinder.java
+++ Exam2/src/main/java/com/csc161j/Exam2/FileSizeFinder.java
@@ -1,23 +1,38 @@
 package com.csc161j.Exam2;
 
 import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class FileSizeFinder {
 
 	public static void main(String[] args) {
 
-		String filePath = "D:\\FileSizeTest";
-		
+		Scanner scnr = new Scanner(System.in);
+		Path currentDirectoryPath = FileSystems.getDefault().getPath("");
+    	String currentDirectoryName = currentDirectoryPath.toAbsolutePath().toString(), filePath = "", input = "";
+    	String[] directorySegments = currentDirectoryName.split("\\\\");
+    	for (int i = -1, j = 0; i <= 1; j++) {
+    		if (i != -1) {filePath += "\\" + directorySegments[j];}
+    		else {i++; filePath += directorySegments[j];}
+    		if (directorySegments[j].equals("Users") || i == 1) {i++;}
+    	}
+    	filePath += "\\Downloads\\FileSizeTest\\FileSizeTest";
+    	System.out.println("Enter file path or leave blank for default");
+    	input = scnr.nextLine();
+    	if (!input.equals("")) {filePath = input;}
+
 		File file = new File(filePath);
-		
+
 		if (!file.exists()) {
 			System.out.println("The file " + filePath + " does not exist.\nExiting ...");
 			System.exit(-1);
 		}
-		
+
 		FileTree tree = new FileTree(filePath);
-		
+
 		System.out.println("File size listing");
 		for (FileNode fileNode: tree) {
 			if (fileNode.getFile().isDirectory()) {
@@ -29,7 +44,7 @@ public class FileSizeFinder {
 				System.out.println(fileNode);
 			}
 		}
-		
+
 		System.out.println("===============================================================================================");
 		System.out.println("Breadth first listing of files");
 		Iterator<FileNode> iterator = tree.breadthFirstIterator();
