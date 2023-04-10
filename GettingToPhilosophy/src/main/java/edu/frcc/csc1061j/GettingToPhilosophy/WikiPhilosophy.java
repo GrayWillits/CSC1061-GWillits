b2d8c08b6860f63af17fc772a95c33bee9d203ca
Gray Willits <jacobpwillits@gmail.com>
2023-04-10 16:01:04 -0600

    finished Getting To Philosophy

--- GettingToPhilosophy/src/main/java/edu/frcc/csc1061j/GettingToPhilosophy/WikiPhilosophy.java
+++ GettingToPhilosophy/src/main/java/edu/frcc/csc1061j/GettingToPhilosophy/WikiPhilosophy.java
@@ -2,7 +2,9 @@ package edu.frcc.csc1061j.GettingToPhilosophy;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -34,6 +36,17 @@ public class WikiPhilosophy {
         String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 
         testConjecture(destination, source, 20);
+        
+        for (String site: visited) {
+        	System.out.println(site);
+        }
+        
+        if (visited.contains(destination)) {
+        	System.out.println("\n\nSuccess");
+        }
+        else {
+        	System.out.println("\n\nFailure");
+        }
     }
 
     /**
@@ -44,6 +57,14 @@ public class WikiPhilosophy {
      * @throws IOException
      */
     public static void testConjecture(String destination, String source, int limit) throws IOException {
+    	visited.add(source);
+    	if (limit <= 0) {
+    		return;
+    	}
+    	if (source.equals(destination)) {
+    		return;
+    	}
+    	
         Document doc = null;
         Connection conn = Jsoup.connect(source);
         try 
@@ -63,9 +84,31 @@ public class WikiPhilosophy {
 
         for (Element para : paragraphs) {
             Iterable<Node> iter = new WikiNodeIterable(para);
+            Stack<String> paren = new Stack<String>();
             for (Node node : iter) {
+            	if (node.toString().contains("(") && node.toString().charAt(0) != '<') {
+            		String temp = new String(node.toString());
+            		for (int i = 0; i < node.toString().length() - (new String(node.toString())).replace("(", "").length(); i++) {
+            			paren.push("(");
+            		}
+            	}
+            	if (node.toString().contains(")") && node.toString().charAt(0) != '<') {
+            		for (int i = 0; i < node.toString().length() - (new String(node.toString())).replace(")", "").length(); i++) {
+            			paren.pop();
+            		}
+            	}
+            	
+            	if (paren.size() == 0 && node.toString().length() > 1 && node.toString().charAt(0) == '<' && node.toString().charAt(1) == 'a' && node.toString().contains("href")) {
+            		String path = node.toString().split("\"")[1];
+            		if (path.charAt(0) != '#' && !visited.contains("https://en.wikipedia.org" + path)) {
+            			testConjecture(destination, ("https://en.wikipedia.org" + path), limit -1 );
+            			return;
+            		}
+            	}
                 
                 // TODO: FILL THIS IN!
+            	
+            	
             	// If this node is a text node make sure you are not within parentheses
             	
             	// If this node has a link you can get it by accessing the href attribute in the node
